1dfbe9054e2310bbad698d2cd0007384c6a1d428
aya <aya.elgamal14@gmail.com>
2021-06-03 01:18:55 +0200

    some extra comments and using assign

--- src/OS.java
+++ src/OS.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Hashtable;
 
 public class OS {
@@ -20,37 +21,47 @@ public class OS {
         }
     }
 
-    private static void print(Object x) {
-        System.out.println(x.toString());
+    private static void print(String x) {
+        if(variables.contains(x)) System.out.println(variables.get(x));
+        else System.out.println(x);
     }
 
 
     private static void assign(String variable, Object value) {
         variables.put(variable, value);
-//        variables.replace(words[1], result);
-
     }
 
     private static void add(String x, String y) {
+        //todo extract from variables parse then add in res
+        int res=0;
+        assign(x,res);
     }
 
     private static void writeFile(String fileName, String data) {
+        //todo if does not exist filename create
     }
-
 
     private static String readFile(String fileName) {
-        return null; //todo reading
+        return null; //todo reading from file
     }
 
-    public static void interpret(String s) {
+    public static void interpret(String s) throws IOException {
 
         String[] words = s.split(" ");
         switch (words[0]) {
             case ("assign"): {
-                Object result = words[2];
-                if (words[2].equals("readFile")) {
-                    String fileName = words[3];
-                    result = readFile(fileName);
+                Object result ;
+                switch (words[2]) {
+                    case ("input"): {
+                        result = input();
+                        break;
+                    }
+                    case ("readFile"): {
+                        String fileName = words[3];
+                        result = readFile(fileName);
+                        break;
+                    }
+                    default:result=words[2];
                 }
                 assign(words[1], result);
                 break;
@@ -60,12 +71,17 @@ public class OS {
                 break;
             }
             case ("print"): {
-                print(variables.get(words[1]));
+                print(words[1]);
                 break;
             }
         }
 
 
+    }
+
+    private static String input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        return br.readLine();
     }
 
 }
