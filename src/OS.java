7828c01c9fe4b5593621ab776f98f0abd15f2585
BASANT <basant_allam@hotmail.com>
2021-06-02 23:40:55 +0200

    main

--- src/OS.java
+++ src/OS.java
@@ -1,12 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class OS {
 
     public static void main (String[] args){
         String programPath="";
-        BufferedReader br = new BufferedReader(new FileReader(programPath))
-        while(br.hasNext()){
+        
+        BufferedReader br = new BufferedReader(new FileReader(programPath));
+        while(br.ready()){
             interpret(br.readLine());
         }
-    }
+    }	
 
     public static void interpret(String s){
 
