587024b45a47088dd2662876d63beab41182b507
aya <aya.elgamal14@gmail.com>
2021-06-02 23:24:43 +0200

    Interface

--- src/OS.java
+++ src/OS.java
@@ -1,2 +1,25 @@
+import java.util.Hashtable;
+
 public class OS {
+    Hashtable<String,String> varString= new Hashtable<>();
+    Hashtable<String,Integer> varInt= new Hashtable<>();
+
+    private static void print() {
+    }
+
+    private static void assign() {
+    }
+
+    private static void add() {
+    }
+
+    private static void writeFile() {
+    }
+
+    private static void readFile() {
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
