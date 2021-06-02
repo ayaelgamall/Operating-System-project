ef4d28bb299b2f97be96d35dd48e193ae776cfc8
aya <aya.elgamal14@gmail.com>
2021-06-03 00:52:45 +0200

    assign

--- src/OS.java
+++ src/OS.java
@@ -1,14 +1,21 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Hashtable;
+
 public class OS {
 
-   static Hashtable<String,Object> variables= new Hashtable<>();
+    static Hashtable<String, Object> variables = new Hashtable<>();
 
-    public static void main (String[] args)throws Exception{
-        String programPath="";
+    public static void main(String[] args) throws Exception {
+        execute("Program 1.txt");
+        execute("Program 2.txt");
+        execute("Program 3.txt");
+    }
+
+    private static void execute(String programPath) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(programPath));
-        while(br.ready()){
+        while (br.ready()) {
             interpret(br.readLine());
         }
     }
@@ -18,14 +25,16 @@ public class OS {
     }
 
 
-    private static void assign(String variable,Object value) {
-        variables.put(variable,value);
+    private static void assign(String variable, Object value) {
+        variables.put(variable, value);
+//        variables.replace(words[1], result);
+
     }
 
-    private static void add(String x, String y ) {
+    private static void add(String x, String y) {
     }
 
-    private static void writeFile(String fileName,String data) {
+    private static void writeFile(String fileName, String data) {
     }
 
 
@@ -38,23 +47,16 @@ public class OS {
         String[] words = s.split(" ");
         switch (words[0]) {
             case ("assign"): {
-                Object result = null;
-                switch (words[2]) {
-                    case ("input"): {
-                        result = input();
-                        break;
-                    }
-                    case ("readFile"): {
-                        String fileName = words[3];
-                        result = readFile(fileName);
-                    }
+                Object result = words[2];
+                if (words[2].equals("readFile")) {
+                    String fileName = words[3];
+                    result = readFile(fileName);
                 }
-                //should parse el integers abl ma save
-                variables.replace(words[1], result);
+                assign(words[1], result);
                 break;
             }
             case ("writeFile"): {
-                writeFile(words[1],words[2]);
+                writeFile(words[1], words[2]);
                 break;
             }
             case ("print"): {
@@ -64,10 +66,6 @@ public class OS {
         }
 
 
-    }
-    public static String input(){
-        //todo
-        return null;
     }
 
 }
