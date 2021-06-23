6ddd970412ab6621629a1a0bde68f42edc438d1f
BASANT <basant_allam@hotmail.com>
2021-06-23 22:58:22 +0200

    struct

--- src/OS.java
+++ src/OS.java
@@ -1,15 +1,13 @@
 import java.io.*;
 import java.util.Hashtable;
-
 public class OS {
 
     private static Hashtable<String, String> variables = new Hashtable<>();
-    private static Word [] memory; //3yzen n5leha pairs aw 7aga + fixed size
-    private static int IDs =0; //increment w each process
+    private static Word [] memory= new Word[1000];
+    private static int PID =0; //increment with each process
     static int PC = 0; //idk hngebo mnen
-    public static int index;
-   
-    
+    public static int index=-1;
+
     public static void print(String x) {
         System.out.println(readMemory(x));
     }
@@ -19,7 +17,6 @@ public class OS {
         return variables.getOrDefault(x, x);
     }
 
-
     public static void assign(String variable, String value) {
         writeMemory(variable, value);
     }
@@ -28,14 +25,12 @@ public class OS {
          variables.put(variable, value);
     }
 
-
     public static void add(String x, String y) {
         int i1 = Integer.parseInt(readMemory(x));
         int i2 = Integer.parseInt(readMemory(y));
         int res= i1 + i2;
         writeMemory(x,""+res);
     }
-
 
     public static void writeFile(String fileNameVar, String dataVar) throws IOException {
         String fileName= readMemory(fileNameVar);
@@ -65,43 +60,33 @@ public class OS {
         } catch (Exception e) {
             print("File Does not Exist");
         }
-
         return data.toString();
     }
-
-    
 
     public static String input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         return br.readLine();
     }
 
-
-//
-//
-//    public static void initializePCB(String process) {
-//        // change the way the info is stored when we change the mem array type
-//        //++index wla index++   ???
-//        memory[index] = "lower Boundary:"+ index;
-//        memory[++index] = "startPC:" + PC;
-//        memory[++index] = "ID:" + assignID();
-//        memory[++index] = "State:" + state.NotRunnig ;
-//        memory[++index] = "Upper Boundary:" + index; //lw b2et mem[index++] 5ly el boundaryS index++
-//
-//    }
-//    public static int assignID() {
-//        return IDs++;
-//    }
-//    public static void main(String[] args) {
-//        //read processes in order
-//        //call initializeProcess for each one
-//        //store variables (in memory brdo)
-//        //after doing that for all 3 processes
-//        //start scheduler
-//        //2 instruction for each process then preemt
-//        //keep going till all processes finished
-//    }
-
+    public static void initializePCB(String process) {
+        memory[++index] = new Word("lower Boundary", index+1);
+        memory[++index] =  new Word("startPC",PC);
+        memory[++index] =  new Word("ID:", assignID());
+        memory[++index] =  new Word("State", state.NotRunnig);
+        memory[++index] =  new Word("Upper Boundary",index+1); //lw b2et mem[index++] 5ly el boundaryS index++
+    }
+    public static int assignID() {
+        return PID++;
+    }
+    public static void main(String[] args) {
+        //read processes in order
+        //call initializeProcess for each one
+        //store variables (in memory brdo)
+        //after doing that for all 3 processes
+        //start scheduler
+        //2 instruction for each process then preemt
+        //keep going till all processes finished
+    }
 
     public enum state {
         Running,
@@ -110,11 +95,11 @@ public class OS {
     }
 
     public static class Word {
-        String type;
+        String key;
         Object value;
-        public Word(String t, Object v){
+        public Word(key k, Object v){
             value=v;
-            type=t;
+            key=k;
         }
 
     }
